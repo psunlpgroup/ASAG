348d512c4851688153acd5ff1c4fc278c1217b91
Zhaohui Li <nkzhaohuilee@gmail.com>
2023-10-26 14:08:07 -0400

    Update DataModule.py

--- DataModule.py
+++ DataModule.py
@@ -1,53 +1,77 @@
-import csv
-import spacy
 import torch
+import csv
 from torch.utils.data import Dataset
 
-# Try running python -m spacy download en and it should install the model to the correct directory.
+from Constants import *
 
-class LRDataset(Dataset):
-    def __init__(self, answer_file_path, max_length):
+class SequenceDataset(Dataset):
+    def __init__(self, dataset_file_path, tokenizer, device):
         # Read JSON file and assign to headlines variable (list of strings)
         self.data_dict = []
+        self.device = device
         self.lable_set = set()
-        self.token2id = {}
-        self.max_length = max_length
-        # prepare data
-        self.token_set, datas = self.load_data(answer_file_path)
-        self.data_dict = self.process_data(datas)
-        self.token2id = self.set2id(self.token_set, 'PAD', 'UNK')
-        self.tag2id = self.set2id(set((datas).keys()))
-        # create_dataset
-
-
+        file_data = []
+        for file in dataset_file_path:
+            with open(file) as csvfile:
+                csv_reader = csv.reader(csvfile)
+                file_header = next(csv_reader)
+                for row in csv_reader:
+                    file_data.append(row)
+                for row in file_data:
+                    #col-stat
+                    cat = row[-1]
+                    q_id = row[2]
+                    #print(row[0])
+                    q_text = q_text_dict[q_id]
+                    # print(refs)
+                    data_list = []
+                    ref_list = []
+                    if len(q_rubric_dict[q_id])>1:
+                        ref_list = q_rubric_dict[q_id]
+                    else:
+                        ref_list.append(q_rubric_dict[q_id][0])
+                        ref_list.append(q_rubric_dict[q_id][0])
+                    for t in ref_list:
+                        line = CLS_TOKEN + row[1] + SEP_TOKEN + t + SEP_TOKEN + q_text
+                        #line = CLS_TOKEN + q_text + SEP_TOKEN + t + SEP_TOKEN + row[1]
+                        #line = q_text + SEP_TOKEN + t + SEP_TOKEN + row[1] # test5
+                        #line = t + SEP_TOKEN + row[1] # test6
+                        #line = row[1] # test7
+                        data_list.append(line)
+                    # assert 0 == 1
+                    data = []
+                    self.lable_set.add(cat)
+                    data.append(cat)
+                    data.append(data_list)
+                    self.data_dict.append(data)
+        self.tokenizer = tokenizer
+        self.tag2id = self.set2id(self.lable_set)
+        #self.tag2id = {'0': 0, '1': 1, '2': 2, '3': 3}
+        print(self.tag2id)
+        print(self.get_category_distribution())
 
     def __len__(self):
         return len(self.data_dict)
 
     def __getitem__(self, index):
-        line, label = self.data_dict[index]
+        DEVICE = self.device
+        label, lines = self.data_dict[index]
+        label = self.tag2id[label]
+        input_ids, attention_masks = [], []
+        #print(lines)
+        for line in lines:
+            #tokenized_data = self.tokenizer(line, padding="max_length", truncation=True, return_tensors="pt")
+            tokenized_data = self.tokenizer(line, padding="max_length", truncation=True, max_length=hyperparameters['max_length'])
+            input_id = tokenized_data["input_ids"]
+            attention_mask = tokenized_data["attention_mask"]
+            input_ids.append(input_id)
+            attention_masks.append(attention_mask)
+        return {
+            "input_ids":  torch.tensor(input_ids, dtype=torch.long, device=self.device),
+            "attention_mask": torch.tensor(attention_masks, dtype=torch.long, device=self.device),
+            "label": label,
+        }
 
-        seq_vector = self.vectorized_data(line, self.token2id)
-        #print("seq_vector : {}".format(seqs_tensor.size()))
-        seq_lengths = torch.LongTensor([len(seq_vector)])
-        # padding
-        padding_length = self.max_length - len(seq_vector)
-        senten_vec = seq_vector + [0] * padding_length
-        seqs_tensor = torch.LongTensor(senten_vec)
-        # label
-        label_tensor = torch.LongTensor([self.tag2id[label]])
-
-        # print("seqs_tensor : {}".format(seqs_tensor.size()))
-        # print(seqs_tensor)
-        #
-        # print("seq_lengths : {}".format(seq_lengths.size()))
-        # print(seq_lengths)
-        #
-        # print("label_tensor : {}".format(label_tensor.size()))
-        # print(label_tensor)
-        #assert 1 == 0
-
-        return seqs_tensor, label_tensor, seq_lengths
 
     def set2id(self, item_set, pad=None, unk=None):
         item2id = {}
@@ -60,78 +84,13 @@ class LRDataset(Dataset):
             item2id[item] = len(item2id)
 
         return item2id
-
-    def load_data(self, data_file):
-        tok = spacy.load('en')
-        token_set = set()
-
-        datas = {}
-        # Train Set
-        file_data = []
-        for file in data_file:
-            print(file)
-            with open(file) as csvfile:
-                csv_reader = csv.reader(csvfile)
-                file_header = next(csv_reader)
-                for row in csv_reader:
-                    file_data.append(row)
-
-        for row in file_data:
-            cat = row[3]
-            q_id = row[2]
-            data = []
-
-            line = row[1]
-            #print(line)
-            line = [token.text for token in tok.tokenizer(line)]
-            for token in line:
-                token_set.add(token)
-            if len(line) > (self.max_length - 2):
-                print("Exceed the max length")
-                line = line[0:self.max_length - 3]
-            data.append(line)
-            if cat in datas.keys():
-                datas[cat].append(data)
-            else:
-                datas[cat] = []
-                datas[cat].append(data)
-
-        return token_set, datas
-
-    def process_data(self, datas):
-        """
-        Split data into train, dev, and test (currently use 80%/10%/10%)
-        It is more make sense to split based on category, but currently it hurts performance
-        """
-        print('Data statistics: ')
-        all_data = []
-        for cat in datas:
-            cat_data = datas[cat]
-            print(cat, len(datas[cat]))
-            # for dat in cat_data:
-            #   print((dat, cat))
-            all_data += [(dat, cat) for dat in cat_data]
-
-        train_cat = set()
-        for item, cat in all_data:
-            train_cat.add(cat)
-        print('Categories:', sorted(list(train_cat)))
-        return all_data
-
-    def vectorized_data(self, data, item2id):
-        data_vec = []
-        for seq in data:
-            #print(seq)
-            senten_vec = []
-            # text
-            for token in seq:
-                if token in item2id:
-                    senten_vec.append(item2id[token])
-                else:
-                    senten_vec.append(item2id['UNK'])
-            data_vec.append(senten_vec)
-
-        #print(data_vec)
-        return data_vec[0]
-
-
+    def get_category_distribution(self):
+        cat_count = {}
+        for data in self.data_dict:
+            cat = data[0]  # Assuming category is the first element in the list
+            #print(cat)
+            if cat not in cat_count:
+                cat_count[cat] = 0
+            cat_count[cat] += 1
+        #print(cat_count)
+        return cat_count
